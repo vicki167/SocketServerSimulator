ad5f5f0e71c1de93eedcd50858102033639008ac
John Johnson <john@cavaliersoftware.com>
2017-12-16 04:02:41 -0500

    Initial commit of project structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Socket Server Simulator
+
+Simple socket server for generating and sending sample binary test data
+
+## Build instructions
+
+'gradle build'
+
+## Executing
+
+Load in your favorite IDE and run the Application class or execute the built jar file with 
+'java -jar ' 
---
+++ src/main/java/com/cavaliersoftware/sample/Application.java
@@ -0,0 +1,15 @@
+package com.cavaliersoftware.sample;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john
+ * Date: 12/16/17
+ * Time: 3:59 AM
+ * <p>
+ */
+public class Application {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello, World!" );
+    }
+}
